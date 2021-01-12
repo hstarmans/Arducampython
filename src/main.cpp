cd755d61f10116c07498a5f6a3c426faf91e456b
Rik Starmans <info@hexastorm.com>
2021-01-12 09:28:43 +0000

    forgot to add modifieed main

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,8 @@ PYBIND11_MODULE(camera, m) {
         .def("init", &Camera::init)
         .def("close", &Camera::close)
         .def("live_view", &Camera::live_view, "Capture image with given exposure time and return buffer", py::arg("exptime"))
-        .def("capture", &Camera::capture, py::return_value_policy::reference, "Capture image with given exposure time and return buffer", py::arg("exptime"))
+        .def("capture", &Camera::pythoncapture,
+		 py::return_value_policy::copy, 
+			"Capture image with given exposure time and return buffer", py::arg("exptime"))
         .def("set_mode", &Camera::set_mode, "Set the camera mode", py::arg("mode"));
 }
